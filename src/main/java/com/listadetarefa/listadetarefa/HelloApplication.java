78803494701713aaf7679dd17bf5441daf3d183c
CarlosT098 <carlostgt1590@gmail.com>
2024-09-07 10:51:42 -0300

    atualizacao do botao de salvar

--- src/main/java/com/listadetarefa/listadetarefa/HelloApplication.java
+++ src/main/java/com/listadetarefa/listadetarefa/HelloApplication.java
@@ -13,12 +13,16 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelloApplication extends Application {
     
+    /* 
     @Override
     public void start(Stage primaryStage) throws IOException{
         // Criação de um array de exemplo
-        String[] items = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5"};
+        ArrayList<String> items = ler();
 
         // Criação de um VBox para armazenar as Labels
         VBox vbox = new VBox();
@@ -38,8 +42,9 @@ public class HelloApplication extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+        */
 
-    /* 
+    
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("index-view.fxml"));
@@ -48,17 +53,17 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-    */
+    
 
     public static void main(String[] args) {
-        salvar();
-        ler();
         launch();
     }
 
-    public static void salvar(){
-        String nome = "João";
-        int idade = 25;
+    public void salvar(List<Tarefa> listaTarefa){
+
+        int c = 0;
+
+        listaTarefa.get(0).getTitulo();
 
         String[] nomes = {"joao", "maria", "douglas", "clara", "amanda"};
 
@@ -66,11 +71,11 @@ public class HelloApplication extends Application {
             // Criando um FileWriter para escrever no arquivo "dados.txt"
             FileWriter writer = new FileWriter("dados.txt");
             
-            // Escrevendo as variáveis no arquivo
-            for(int i = 0; i<=4; i++){
-                writer.write(nomes[i] + "," + idade + ",");
+            // Escrevendo as variáveis no arquivo            
+            while (listaTarefa.size() > c) {
+                writer.write(listaTarefa.get(c).getTitulo() + ",");
+                c++;
             }
-            
             //writer.write(nome + "," + idade);
             
             // Fechando o FileWriter
@@ -83,7 +88,7 @@ public class HelloApplication extends Application {
         }
     }
 
-    public static void ler(){
+    public static ArrayList<String> ler(){
         try {
             // Criando um BufferedReader para ler o arquivo "dados.txt"
             BufferedReader reader = new BufferedReader(new FileReader("dados.txt"));
@@ -92,21 +97,22 @@ public class HelloApplication extends Application {
             String linha = reader.readLine();
             
             // Separando os dados (nome e idade) pelo caractere ","
+            ArrayList<String> nomes = new ArrayList<String>();
             String[] dados = linha.split(",");
-            String nome;
             int idade;
             for(int i = 0; i<=4; i++){
-                nome = dados[i];
+                nomes.add(dados[i]);
                 i++;
                 idade = Integer.parseInt(dados[i]);
                 reader.close();
-                System.out.println("Nome: " + nome);
-                System.out.println("Idade: " + idade);
             }
+            return nomes;
 
         } catch (IOException e) {
             System.out.println("Ocorreu um erro ao carregar os dados.");
             e.printStackTrace();
+            return null;
+
         }
     }
 
--- src/main/java/com/listadetarefa/listadetarefa/TarefaController.java
+++ src/main/java/com/listadetarefa/listadetarefa/TarefaController.java
@@ -1,10 +1,19 @@
 package com.listadetarefa.listadetarefa;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import java.io.IOException;
+
 
 public class TarefaController {
     @FXML
@@ -20,11 +29,19 @@ public class TarefaController {
     private Spinner<Integer> spImportancia;
 
     @FXML
-    public void btnSalvar(){
+    public void btnSalvar(ActionEvent event) throws IOException {
         Tarefa tarefa = new Tarefa(tfTitulo.getText(), tfDescricao.getText(), spImportancia.getValue());
         List<Tarefa> listaTarefa = new ArrayList<Tarefa>();
         listaTarefa.add(tarefa);
         teste.setText("Salvo!");
+        HelloApplication test = new HelloApplication();
+        test.salvar(listaTarefa);
+
+        Parent pagina2Parent = FXMLLoader.load(getClass().getResource("index-view.fxml"));
+        Scene pagina2Scene = new Scene(pagina2Parent);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(pagina2Scene);
+        stage.show();
     }
 
 }
