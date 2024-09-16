4b81c3b34ccdfedd6f311b384e8a05655b3a3a7c
CarlosT098 <carlostgt1590@gmail.com>
2024-09-16 13:18:11 -0300

    ler o arquivo txt e mostrar na tela principal

--- src/main/java/com/listadetarefa/listadetarefa/HelloApplication.java
+++ src/main/java/com/listadetarefa/listadetarefa/HelloApplication.java
@@ -2,6 +2,7 @@ package com.listadetarefa.listadetarefa;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
@@ -12,27 +13,35 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-
+import java.net.URL;
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
+;
+
 public class HelloApplication extends Application {
     
-    /* 
+    
     @Override
     public void start(Stage primaryStage) throws IOException{
         // Criação de um array de exemplo
-        ArrayList<String> items = ler();
+        ArrayList<String> items = new ArrayList<>();
+        items.addAll(ler());
 
         // Criação de um VBox para armazenar as Labels
         VBox vbox = new VBox();
 
         // Percorrer o array e adicionar uma Label para cada item
-        for (String item : items) {
-            Label label = new Label(item);
-            CheckBox cb = new CheckBox(item);
-            vbox.getChildren().add(cb); // Adiciona a label no VBox
+        int c = 0;
+        while (items.size() > c) {
+            Label label = new Label(items.get(c));
+            CheckBox cb = new CheckBox(items.get(c));
+            vbox.getChildren().add(label); // Adiciona a label no VBox
+            c++;
         }
+
+        
 
         // Configuração da cena com o VBox
         Scene scene = new Scene(vbox, 300, 200);
@@ -42,17 +51,18 @@ public class HelloApplication extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-        */
+        
 
-    
+    /*
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("index-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("index-view.fxml"));    
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("index");
         stage.setScene(scene);
         stage.show();
     }
+         */
     
 
     public static void main(String[] args) {
@@ -99,11 +109,11 @@ public class HelloApplication extends Application {
             // Separando os dados (nome e idade) pelo caractere ","
             ArrayList<String> nomes = new ArrayList<String>();
             String[] dados = linha.split(",");
-            int idade;
+            
             for(int i = 0; i<=4; i++){
                 nomes.add(dados[i]);
                 i++;
-                idade = Integer.parseInt(dados[i]);
+                
                 reader.close();
             }
             return nomes;
